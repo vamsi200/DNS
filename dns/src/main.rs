7597c85b53820553852bb3ee1d6efeb6433f237c
vamsi200 <vamsivanama891@gmail.com>
2025-02-07 15:18:35 +0530

    Day-5

--- dns/src/main.rs
+++ dns/src/main.rs
@@ -613,10 +613,7 @@ impl DnsPacket {
     }
 }
 
-fn main() -> Result<(), Box<dyn Error>> {
-    let qname = "yahoo.com";
-    let qtype = QueryType::A;
-
+fn lookup(qname: &str, qtype: QueryType) -> Result<DnsPacket, Box<dyn Error>> {
     let server = ("8.8.8.8", 53);
 
     let socket = UdpSocket::bind(("0.0.0.0", 43210))?;
@@ -632,26 +629,70 @@ fn main() -> Result<(), Box<dyn Error>> {
 
     let mut req_buffer = BytePacketBuffer::new();
     packet.write(&mut req_buffer)?;
-
     socket.send_to(&req_buffer.buf[0..req_buffer.pos], server)?;
 
     let mut res_buffer = BytePacketBuffer::new();
     socket.recv_from(&mut res_buffer.buf)?;
 
-    let res_packet = DnsPacket::from_buffer(&mut res_buffer)?;
-    println!("{:#?}", res_packet.header);
+    DnsPacket::from_buffer(&mut res_buffer)
+}
 
-    for q in res_packet.questions {
-        println!("{:#?}", q);
+fn handle_query(socket: &UdpSocket) -> Result<(), Box<dyn Error>> {
+    let mut req_buffer = BytePacketBuffer::new();
+    let (_, src) = socket.recv_from(&mut req_buffer.buf)?;
+
+    let mut request = DnsPacket::from_buffer(&mut req_buffer)?;
+
+    let mut packet = DnsPacket::new();
+
+    packet.header.id = request.header.id;
+    packet.header.recursion_desired = true;
+    packet.header.recursion_available = true;
+    packet.header.response = true;
+
+    if let Some(question) = request.questions.pop() {
+        println!("Received Query {:?}", question);
+
+        if let Ok(result) = lookup(&question.name, question.qtype) {
+            packet.questions.push(question);
+            packet.header.rescode = result.header.rescode;
+
+            for rec in result.answers {
+                println!("Answer: {:?}", rec);
+                packet.answers.push(rec);
+            }
+            for rec in result.authorities {
+                println!("Authority: {:?}", rec);
+                packet.authorities.push(rec);
+            }
+            for rec in result.resources {
+                println!("Resources: {:?}", rec);
+                packet.resources.push(rec);
+            }
+        } else {
+            packet.header.rescode = ResultCode::SERVFAIL;
+        }
+    } else {
+        packet.header.rescode = ResultCode::FORMERR;
     }
-    for rec in res_packet.answers {
-        println!("{:#?}", rec);
-    }
-    for rec in res_packet.authorities {
-        println!("{:#?}", rec);
-    }
-    for rec in res_packet.resources {
-        println!("{:#?}", rec);
-    }
+
+    let mut res_buffer = BytePacketBuffer::new();
+    packet.write(&mut res_buffer)?;
+
+    let len = res_buffer.pos();
+    let data = res_buffer.get_buf_range(0, len)?;
+
+    socket.send_to(data, src)?;
+
     Ok(())
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let socket = UdpSocket::bind(("0.0.0.0", 2053))?;
+    loop {
+        match handle_query(&socket) {
+            Ok(_) => {}
+            Err(e) => eprintln!("An error occurred: {}", e),
+        }
+    }
 }
